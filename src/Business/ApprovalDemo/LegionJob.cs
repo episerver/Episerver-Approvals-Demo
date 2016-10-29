7a9ff1d4ee5f3963f4564b2b18043a3860365992
John-Philip Johansson <john-philip.johansson@episerver.com>
2016-10-29 17:05:19 +0200

    Its name is Legion, and they are many.

---
+++ src/Business/ApprovalDemo/LegionJob.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using EPiServer;
+using EPiServer.Approvals;
+using EPiServer.Core;
+using EPiServer.PlugIn;
+using EPiServer.Scheduler;
+using EPiServer.ServiceLocation;
+
+namespace Ascend2016.Business.ApprovalDemo
+{
+    [ScheduledPlugIn(DisplayName = "Legion")]
+    public class LegionJob : ScheduledJobBase
+    {
+        private const string Username = "gandalf";
+        private Injected<IApprovalEngine> _approvalEngine;
+        private Injected<IApprovalRepository> _approvalRepository;
+        private Injected<IContentRepository> _contentRepository;
+
+        private string DoJob()
+        {
+            var linguo = new SpellCheckApprover();
+
+            // Get all approvals waiting for user to approve
+            var query = new ApprovalsQuery
+            {
+                Status = ApprovalStatus.Pending,
+                Username = linguo.Username
+            };
+            var approvals = _approvalRepository.Service.ListAsync(query).Result;
+
+            if (!approvals.Any())
+            {
+                return $"No approvals for {query.Username} to approve.";
+            }
+
+            // Spell check all of them
+            var approved = 0;
+            var rejected = 0;
+            foreach (var approval in approvals)
+            {
+                var page = _contentRepository.Service.Get<PageData>(approval.ContentLink);
+                var decision = linguo.DoDecide(page);
+
+                if (decision == ApprovalStatus.Approved)
+                {
+                    // TODO: Remove the ApprovalDecisionScope param when updating to latest Approvals API
+                    _approvalEngine.Service.ApproveAsync(approval.ID, Username, approval.ActiveStepIndex,
+                        ApprovalDecisionScope.Step).Wait();
+                    approved++;;
+                }
+                else if (decision == ApprovalStatus.Rejected)
+                {
+                    // TODO: Remove the ApprovalDecisionScope param when updating to latest Approvals API
+                    _approvalEngine.Service.RejectAsync(approval.ID, Username, approval.ActiveStepIndex,
+                        ApprovalDecisionScope.Step).Wait();
+                    rejected++;
+                }
+            }
+
+            return $"Legion reports {approved} approvals and {rejected} rejections.";
+        }
+
+        #region Not important for Content Approvals API demonstration
+
+        private bool _stopSignaled;
+
+        public LegionJob()
+        {
+            IsStoppable = true;
+        }
+
+        public override void Stop()
+        {
+            _stopSignaled = true;
+        }
+
+        public override string Execute()
+        {
+            // Call OnStatusChanged to periodically notify progress of job for manually started jobs
+            OnStatusChanged($"Starting execution of {GetType()}");
+
+            // Add implementation
+            var jobResult = DoJob();
+
+            // For long running jobs periodically check if stop is signaled and if so stop execution
+            if (_stopSignaled)
+            {
+                return "Stop of job was called";
+            }
+
+            return jobResult;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Business/ApprovalDemo/SpellCheckApprover.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Ascend2016.Models.Pages;
+using EPiServer.Approvals;
+using EPiServer.Core;
+using Newtonsoft.Json;
+
+namespace Ascend2016.Business.ApprovalDemo
+{
+    public class SpellCheckApprover
+    {
+        public string Username => "Linguo";
+
+        public ApprovalStatus DoDecide(PageData page)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // Using Bing Spell Check service to look for spelling mistakes.
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key",
+                    System.Configuration.ConfigurationManager.AppSettings["BingSpellCheckKey"]);
+
+                var sitePageData = page as SitePageData;
+                if (sitePageData != null)
+                {
+                    var teaserText = sitePageData.TeaserText;
+                    //teaserText = "Bill Gatas"; // Note: Used to demo a spelling mistake.
+                    var language = page.Language.TextInfo.CultureName;
+
+                    var model = BingSpellChecker(teaserText, language, httpClient);
+
+                    if (model.FlaggedTokens.Any())
+                    {
+                        // TODO: Grab the reasons and use them as the rejection reason.
+                        return ApprovalStatus.Rejected;
+                    }
+                }
+            }
+
+            return ApprovalStatus.Approved;
+        }
+
+        private static BingSpellCheckResponse BingSpellChecker(string text, string language, HttpClient httpClient)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["text"] = text;
+            queryString["mkt"] = language;
+
+            // As long as texts are short enough for a URL query, this is good enough.
+            var uri = $"https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/?{queryString}";
+            var response = httpClient.GetStringAsync(uri).Result;
+            var model = JsonConvert.DeserializeObject<BingSpellCheckResponse>(response);
+
+            //// When texts are too long for a URL query then this is the way to go.
+            //var formContent = new[]
+            //{
+            //    new KeyValuePair<string, string>("text", text)
+            //};
+            //using (var content = new FormUrlEncodedContent(formContent))
+            //{
+            //    var response = httpClient.PostAsync(uri, content).Result;
+            //    var jsonString = response.Content.ReadAsStringAsync().Result;
+            //    var model = JsonConvert.DeserializeObject<BingSpellCheckResponse>(jsonString);
+            //}
+
+            return model;
+        }
+    }
+}
